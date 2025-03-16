f5b323b0e8a93cb1163398013dd2c320e0226d9b
Ernest Kamau <1.kamauernest@gmail.com>
2025-03-16 14:24:32 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+// Remove the <main> element with id "main"
+document.getElementById("main").remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Set the id of newHeader to "victory"
+newHeader.id = "victory";
+
+// Set the text content of newHeader
+newHeader.textContent = "Ernest is the champion";
+
+// Append newHeader to the body
+document.body.appendChild(newHeader);
